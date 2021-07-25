8c213d0a8a5bfbc7861b82f13cefdb2a8070be32
Nimrod Mendel <mendel.nimrod@gmail.com>
2021-07-25 22:19:10 +0300

    configure dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'backend.urls'
 
